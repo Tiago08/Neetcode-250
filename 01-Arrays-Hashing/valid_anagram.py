52dd43ff86d481ddf61bd7087f00d7c547385a34
santibeta <21-10071@usb.ve>
2026-01-12 12:16:02 -0400

    Solve Valid Anagrams

---
+++ 01-Arrays-Hashing/valid_anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            # .get(elemento, valor_por_defecto) evita errores si la llave no existe
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+            
+        return countS == countT
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.isAnagram(s = "bbcc", t = "ccbc"))
