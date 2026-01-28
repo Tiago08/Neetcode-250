f55f1f2dd3e2c94749133d28db140bf02143b651
santibeta <21-10071@usb.ve>
2026-01-28 19:17:08 -0400

    added remove element exercise

---
+++ 01-Arrays-Hashing/remove_element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        k = 0
+    
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.removeElement(nums = [1,1,2,3,4], val = 1))
